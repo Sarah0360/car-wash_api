73e9b466f79a0dbc5cf082bab3863a1d2e74c2a4
sarah <amediorsarah@gmail.com>
2024-08-11 17:41:51 +0000

    service changes

--- Models/services.js
+++ Models/services.js
@@ -4,6 +4,7 @@ import { toJSON } from "@reis/mongoose-to-json";
 const servSchema = new Schema({
     companyName:{type: String, required: true},
     location:{type: String },
+    email: {type: String},
     phoneNumber: {type: string, default: false},
     image: {type: String},
     serviceType: { type: String, enum:['Car-Wash', 'Car-Services']}
--- Schema/services.js
+++ Schema/services.js
@@ -3,6 +3,7 @@ import joi from "joi";
 export const serviceSchema = joi.object({
     companyName:joi.string().required(),
     location:joi.string(),
+    email: joi.string().email(),
     phoneNumber: joi.string(),
     image: joi.string(),
     serviceType: joi.string().valid('Car-Wash', 'Car-Services')
