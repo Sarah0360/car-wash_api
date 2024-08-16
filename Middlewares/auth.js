f6874c3f1471ae13c905b9e5a3654406feca2bcc
sarah <amediorsarah@gmail.com>
2024-08-16 20:28:50 +0000

    email fixes

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -2,17 +2,19 @@ import jwt from "jsonwebtoken";
 
 export const isAuthenticated = (req, res, next) => {
     try {
-                    // ETRACT TOKEN FROM HEADERS
-        const token= req.headers.authorization.split(' ')[1];
+        // ETRACT TOKEN FROM HEADERS
+        const token = req.headers.authorization.split(' ')[1];
         if (token) {
-            
-         // VERIFY THE TOKEN TO GET USER AND APPEND USER TO THE REQUEST
+
+            // VERIFY THE TOKEN TO GET USER AND APPEND USER TO THE REQUEST
             req.user = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
             // CALL NEXT FUNCTION
-            next()
+            next();
         }
-        res.status(401).json({error: 'Token Expired'})
+        else {
+            res.status(401).json({ error: 'Token Expired' })
+        }
     } catch (error) {
-       next(error) 
+        next(error)
     }
 }
--- config/mail.js
+++ config/mail.js
@@ -1,14 +1,14 @@
-import {createTransport} from "nodemailer";
+import { createTransport } from "nodemailer";
 
-export const mailTransport = createTransport ({
+export const mailTransport = createTransport({
     //CREATE A TRANSPORTER
-        //service: process.env.EMAIL_SERVICE,
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        secure: false,  // TRUE IF THE PORT IS 465, FALSE FOR OTHER PORTS 
-        auth: {
-            user: process.env.EMAIL_USER, //SENDER GMAIL ADDRESS
-            pass: process.env.EMAIL_PASSWORD, //APP PASSWORD FROM GMAIL
-        },
-        from: process.env.FROM_EMAIL,
-    });
+    //service: process.env.EMAIL_SERVICE,
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: false,  // TRUE IF THE PORT IS 465, FALSE FOR OTHER PORTS 
+    auth: {
+        user: process.env.EMAIL_USER, //SENDER GMAIL ADDRESS
+        pass: process.env.EMAIL_PASSWORD, //APP PASSWORD FROM GMAIL
+    },
+    from: process.env.FROM_EMAIL,
+});
