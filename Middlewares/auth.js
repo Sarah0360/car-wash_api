8ef937444c4cfe01e40f19e2f6b5594f09c5df19
sarah <amediorsarah@gmail.com>
2024-08-19 20:17:32 +0000

    Booking Update

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const isAuthenticated = (req, res, next) => {
     try {
+        
         // ETRACT TOKEN FROM HEADERS
         const token = req.headers.authorization.split(' ')[1];
         if (token) {
